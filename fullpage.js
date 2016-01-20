6b7ff4350b190af42588f05ccfa7ab8ce6dfe372
andrinheusser <andrinheusser@gmail.com>
2016-01-20 16:57:58 +0100

    clean

--- fullpage.js
+++ fullpage.js
@@ -1,43 +1,6 @@
 "use strict";
 
 import $ from 'jquery';
- 
-/*
-Sample CSS Setup
-
-Basics: Position slides absolute within container and stack them by z-index
-
-.contanier{
-    height: 100vw;
-    width: 100vh;
-
-    & > .slide{
-        height: 0;
-        
-        position: absolute;
-        top: 0; right: 0; bottom: 0; left: 0;
-        width: 100%;
-        overflow: hidden;
-        margin: 0; padding: 0; // avoid margin/paddings on slides
-        
-        transition: height 0.9s ease-in-out;
-
-        &.active{ 
-            height: 100%;
-        }
-
-        // adjust as needed
-        @for $i from 1 through 9{
-            &:nth-child(#{$i}){
-                z-index: 10 - $i;
-            }
-        }
-    }
-
-
-}
-
-*/
 
 var delta,
     currentSlideIndex = 0,
